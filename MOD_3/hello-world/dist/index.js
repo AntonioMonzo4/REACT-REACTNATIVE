0e042bbcf96809d27f327198ba288198dc518ce3
Antonio Monzó <amonansu@gmail.com>
2026-08-01 13:03:25 +0200

    Configuración

---
+++ MOD_3/hello-world/dist/index.js
@@ -0,0 +1,4 @@
+console.log("Hello, World!"); // Basic Ts Hello world 
+let age = 26;
+export {};
+//# sourceMappingURL=index.js.map
---
+++ MOD_3/hello-world/src/index.ts
@@ -0,0 +1,3 @@
+console.log("Hello, World!")// Basic Ts Hello world 
+
+let age: number = 26; 
