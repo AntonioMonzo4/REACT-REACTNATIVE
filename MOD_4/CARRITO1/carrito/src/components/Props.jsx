caff42a572fe9810affee6c6ea14f7d6730eaa2f
Antonio Monzó <amonansu@gmail.com>
2026-08-08 18:43:37 +0200

    Props

---
+++ MOD_4/CARRITO1/carrito/src/components/Props.jsx
@@ -0,0 +1,45 @@
+//COMO USAR LOS PROPS EN REACT
+import React from "react"; 
+
+const Props = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <p>{props.description}</p>
+        </div>
+    )
+}
+
+export default Props
+
+
+//COMO USAR LOS PROPS EN REACT también se puede hacer de la siguiente manera:
+import React from "react";
+
+const Props = ({ title, description }) => {
+    return (
+        <div>
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </div>
+    )
+}
+
+export default Props
+
+//NOTA: Los props son inmutables, es decir, no se pueden modificar desde el componente hijo. Si se quiere modificar un prop, se debe 
+// hacer desde el componente padre.
+//Tambien se puden ponmer como pertenecientes todos a un objeto y desestructurarlos en el componente hijo, de la siguiente manera:
+import React from "react";
+
+const Props = (props) => {
+    const { title, description } = props;
+    return (
+        <div>
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </div>
+    )
+}
+
+export default Props
---
+++ MOD_4/CARRITO1/carrito/src/components/extra.md
@@ -0,0 +1,20 @@
+Para iterear vamops a usar map
+
+
+Esto se incluye en el elmeento parde 
+{data.map(() => {
+    return (
+        <>Aqui ira la información que queremos iterar como objetos o api/ tambien si especificamos un elemento este sera actualizado en rfuncion a la db con los props</>
+
+    )
+})}
+
+Sintaxis de prop en la funcion debemos indicar el objeto parámetro props 
+Puedes acceder a los demas mediante props.price
+
+<Header 
+    nombreDelProp = { datos / state o Funciones}
+    price={},
+    name = 
+
+    />
